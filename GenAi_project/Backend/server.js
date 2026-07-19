10e716bfd76e0065458468c5b7eb3c12436d68fe
Karthik-Atyam <sairamakarthik0210@gmail.com>
2026-07-20 00:21:18 +0530

    updating server.js

--- GenAi_project/Backend/server.js
+++ GenAi_project/Backend/server.js
@@ -2,10 +2,11 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
-connectToDB()
+
 
 async function startServer() {
     try {
+        await connectToDB()
         app.listen(process.env.PORT, () => {
             console.log(`Server is running on port ${process.env.PORT||3000}`);
         });
